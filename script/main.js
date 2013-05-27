0b3c8208ae767ddea8d5737f378c7680fbb24a16
zhulinpinyu <mulixiang199016@gmail.com>
2013-05-27 23:14:26 +0800

    directive

--- script/main.js
+++ script/main.js
@@ -1,7 +1,47 @@
 var app = angular.module('superhero', []);
-app.directive('superman', function(){
+//自定义元素element
+app.directive('supermane', function(){
   return {
     restrict: "E",
     template: "<div>First directive</div>"
   }
 });
+
+//自定义属性
+app.directive('supermana', function(){
+  return {
+    restrict: "A",
+    link: function(){
+      alert('mlx, I am working as attribute!');
+    }
+  }
+});
+
+//自定义class
+app.directive('supermanc', function(){
+  return {
+    restrict: "C",
+    link: function(){
+      alert('mlx,class I am working!');
+    }
+  }
+});
+
+app.directive('superman', function(){
+  return {
+    restrict: "A",
+    link: function(){
+      alert('mlx, I am working!, superman');
+    }
+  }
+});
+
+app.directive('flash', function(){
+  return {
+    restrict: "A",
+    link: function(){
+      alert('mlx, I am working!, flash');
+    }
+  }
+});
+
