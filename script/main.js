f030272eb9e9e9a59633cedfe380b0f2ce030993
zhulinpinyu <mulixiang199016@gmail.com>
2013-05-27 22:59:51 +0800

    first directive

--- script/main.js
+++ script/main.js
@@ -1,37 +1,7 @@
-var myApp = angular.module('myApp', []);
-
-
-myApp.factory('Friend', function(){
-  var friends = {};
-  friends = {mlx:[
-    { 
-      name: "wxb",
-      age: "24"
-    },
-    { 
-      name: "ll",
-      age: "25"
-    },
-    { 
-      name: "lh",
-      age: "24"
-    },
-    { 
-      name: "qjw",
-      age: "23"
-    },
-    { 
-      name: "ft",
-      age: "24"
-    },
-    { 
-      name: "whf",
-      age: "24"
-    }
-  ]};
-  return friends;
+var app = angular.module('superhero', []);
+app.directive('superman', function(){
+  return {
+    restrict: "E",
+    template: "<div>First directive</div>"
+  }
 });
-
-function ListFriendCtrl($scope, Friend){
-  $scope.friends = Friend.mlx
-}
