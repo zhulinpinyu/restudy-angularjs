c42dc604598836102ed20409b122079132588c69
zhulinpinyu <mulixiang199016@gmail.com>
2013-05-30 07:53:59 +0800

    ***16 Understanding isolate Scope***

--- script/main.js
+++ script/main.js
@@ -1,59 +1,18 @@
-var app = angular.module('superApp', []);
+var app = angular.module('choreApp', []);
 
-app.directive('superhero', function(){
+app.controller('choreCtrl',function($scope){
+  $scope.logChore = function(chore){
+    alert(chore);
+  }
+});
+
+app.directive('kid', function(){
   return {
-    restrict: "E",
-    scope: {},
-    controller: function($scope){
-      $scope.info = [];
-      this.addName = function(){
-        $scope.info.push("mlx");
-        console.log("1");
-        console.log($scope.info);
-      }
-      this.addAge = function(){
-        $scope.info.push("23");
-        console.log("2");
-        console.log($scope.info);
-      }
-      this.addAddr = function(){
-        $scope.info.push("wuxi");
-        console.log("3");
-        console.log($scope.info);
-      }
+    restrict: 'E',
+    scope:{
+      done: '&'
     },
-    link: function(scope, element, attrs){
-      element.addClass("btn");
-      element.bind("mouseenter", function(){
-        console.log(scope.info);
-      });
-    }
-  }
-});
-
-app.directive('name', function(){
-  return{
-    require: "superhero",
-    link: function(scope, element, attrs, superheroCtrl){
-      superheroCtrl.addName();
-    }
-  }
-});
-
-app.directive('age', function(){
-  return{
-    require: "superhero",
-    link: function(scope, element, attrs, superheroCtrl){
-      superheroCtrl.addAge();
-    }
-  }
-});
-
-app.directive('addr', function(){
-  return{
-    require: "superhero",
-    link: function(scope, element, attrs, superheroCtrl){
-      superheroCtrl.addAddr();
-    }
+    template: '<input type="text" ng-model="chore"> {{chore}} '+
+      '<div class="btn" ng-click="done({chore:chore})">DONE</div>'
   }
 });
