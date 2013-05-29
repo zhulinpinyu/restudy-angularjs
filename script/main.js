89e7420296f1c05ba4a58db12ce85323cc23aa75
zhulinpinyu <mulixiang199016@gmail.com>
2013-05-29 23:57:10 +0800

    15 Directive to Directive Communication

--- script/main.js
+++ script/main.js
@@ -1,19 +1,59 @@
-var app = angular.module('twitterApp', []);
+var app = angular.module('superApp', []);
 
-app.controller('twitterCtrl',function($scope){
-  $scope.loadMoreTwitters = function(){
-    alert("Twitters Loading...");
-  }
-
-  $scope.deleteTwitters = function(){
-    alert("delete Twitters");
+app.directive('superhero', function(){
+  return {
+    restrict: "E",
+    scope: {},
+    controller: function($scope){
+      $scope.info = [];
+      this.addName = function(){
+        $scope.info.push("mlx");
+        console.log("1");
+        console.log($scope.info);
+      }
+      this.addAge = function(){
+        $scope.info.push("23");
+        console.log("2");
+        console.log($scope.info);
+      }
+      this.addAddr = function(){
+        $scope.info.push("wuxi");
+        console.log("3");
+        console.log($scope.info);
+      }
+    },
+    link: function(scope, element, attrs){
+      element.addClass("btn");
+      element.bind("mouseenter", function(){
+        console.log(scope.info);
+      });
+    }
   }
 });
 
-app.directive('enter', function(){
-  return function(scope, element, attrs){
-    element.bind("mouseenter", function(){
-      scope.$apply(attrs.enter);
-    });
+app.directive('name', function(){
+  return{
+    require: "superhero",
+    link: function(scope, element, attrs, superheroCtrl){
+      superheroCtrl.addName();
+    }
+  }
+});
+
+app.directive('age', function(){
+  return{
+    require: "superhero",
+    link: function(scope, element, attrs, superheroCtrl){
+      superheroCtrl.addAge();
+    }
+  }
+});
+
+app.directive('addr', function(){
+  return{
+    require: "superhero",
+    link: function(scope, element, attrs, superheroCtrl){
+      superheroCtrl.addAddr();
+    }
   }
 });
