9bc50665741d878535f960a453db9c9412434e8a
zhulinpinyu <mulixiang199016@gmail.com>
2013-05-31 23:47:43 +0800

    Transclusion Basics.

--- script/main.js
+++ script/main.js
@@ -6,21 +6,10 @@ app.controller('phoneCtrl', function($scope){
   }
 });
 
-app.directive('phone', function(){
+app.directive('panel', function(){
   return {
     restrict: 'E',
-    scope:{
-      number: '@',
-      network: '=',
-      dial: '&'
-    },
-    template: '<div class="label label-info">Number:{{number}} Say: {{value}}</div>'+
-              '<select ng-model="network" ng-options="n for n in networks"></select>'+
-              '<input type="text" ng-model="value">'+
-              '<div class="btn btn-info" ng-click="dial({number:number,msg:value})">Call Me!</div>',
-    link: function(scope){
-      scope.networks=["CU", "CMCC", "CN"];
-      scope.network = scope.networks[0];
-    }
+    transclude: true,
+    template: '<div class="well" ng-transclude>this is label component!</div>'
   }
 });
