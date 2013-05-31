c1286d7d3c7e98addc5ff6fe2ccdbe87fe325477
zhulinpinyu <mulixiang199016@gmail.com>
2013-06-01 01:07:47 +0800

    23 Thinking Differently About Organization

--- script/main.js
+++ script/main.js
@@ -1,9 +1,23 @@
 var app = angular.module('phoneApp', []);
 
-app.controller('phoneCtrl', function($scope){
+
+var phoneAppStuff = {};
+phoneAppStuff.controllers = {};
+phoneAppStuff.controllers.phoneCtrl = function($scope){
   this.callMe = function(){
     alert("我 Call !");
   }
-
   return $scope.phoneCtrl = this;
-});
+}
+
+phoneAppStuff.directives = {};
+phoneAppStuff.directives.well = function(){
+  return {
+    restrict: 'E',
+    template: '<div class="label">MLX</div>'
+  }
+}
+
+//app.filter();
+app.directive(phoneAppStuff.directives);
+app.controller(phoneAppStuff.controllers);
