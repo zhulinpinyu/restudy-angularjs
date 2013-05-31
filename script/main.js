fd80a023969cc2e5e87db46e1999decfcb90b22c
zhulinpinyu <mulixiang199016@gmail.com>
2013-05-31 23:24:57 +0800

    datepicker timepicker

--- script/main.js
+++ script/main.js
@@ -1,26 +1,5 @@
-var app = angular.module('phoneApp', []);
+var app = angular.module('DatetimeApp', ['$strap.directives']);
 
-app.controller('phoneCtrl', function($scope){
-  $scope.callMe = function(number,msg){
-    alert(number + "  " +msg);
-  }
-});
-
-app.directive('phone', function(){
-  return {
-    restrict: 'E',
-    scope:{
-      number: '@',
-      network: '=',
-      dial: '&'
-    },
-    template: '<div class="label label-info">Number:{{number}} Say: {{value}}</div>'+
-              '<select ng-model="network" ng-options="n for n in networks"></select>'+
-              '<input type="text" ng-model="value">'+
-              '<div class="btn btn-info" ng-click="dial({number:number,msg:value})">Call Me!</div>',
-    link: function(scope){
-      scope.networks=["CU", "CMCC", "CN"];
-      scope.network = scope.networks[0];
-    }
-  }
+app.controller('datetimeCtrl', function($scope){
+  $scope.datepicker = {date: new Date()};
 });
