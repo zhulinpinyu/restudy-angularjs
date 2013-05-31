5a32c9f258226084dc14cb4a692a9c84349f4f6b
zhulinpinyu <mulixiang199016@gmail.com>
2013-05-31 09:25:06 +0800

    ***20 isolate Scope Review***

--- script/main.js
+++ script/main.js
@@ -1,17 +1,26 @@
 var app = angular.module('phoneApp', []);
 
 app.controller('phoneCtrl', function($scope){
-  $scope.callMe = function(msg){
-    alert(msg);
+  $scope.callMe = function(number,msg){
+    alert(number + "  " +msg);
   }
 });
 
 app.directive('phone', function(){
   return {
+    restrict: 'E',
     scope:{
+      number: '@',
+      network: '=',
       dial: '&'
     },
-    template: '<input type="text" ng-model="value">'+
-              '<div class="btn" ng-click="dial({msg:value})">Call Me!</div>'
+    template: '<div class="label label-info">Number:{{number}} Say: {{value}}</div>'+
+              '<select ng-model="network" ng-options="n for n in networks"></select>'+
+              '<input type="text" ng-model="value">'+
+              '<div class="btn btn-info" ng-click="dial({number:number,msg:value})">Call Me!</div>',
+    link: function(scope){
+      scope.networks=["CU", "CMCC", "CN"];
+      scope.network = scope.networks[0];
+    }
   }
 });
