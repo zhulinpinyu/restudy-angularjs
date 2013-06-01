fd94cb269df5e5c14ac0846924e9b8b95fdccec9
zhulinpinyu <mulixiang199016@gmail.com>
2013-06-01 22:01:05 +0800

    angular.element 存在问题

--- script/main.js
+++ script/main.js
@@ -1,17 +1,27 @@
 var app = angular.module('app', []);
 
-
+/*该程序不健壮 存在bug*/
+/*
+ 删除文字在输入 样式不加载 或者 样式加载混乱
+*/
 app.directive('dumbPassword', function(){
-  return {
-    restrict: 'E',
-    replace:true,
-    template: '<div><input type="text" ng-model="model.input"><div>{{model.input}}</div></div>',
-    link: function(scope,element){
+  var validElement = angular.element("<div>{{model.input}}</div>");
+  
+  this.link = function(scope){
       scope.$watch('model.input', function(value){
         if(value=="mlx"){
-          element.children(1).toggleClass("badge badge-success");
+          validElement.toggleClass("badge badge-success");
         }
       });
     }
+
+  return {
+    restrict: 'E',
+    replace:true,
+    template: '<div><input type="text" ng-model="model.input"></div>',
+    compile: function(tElem){
+      tElem.append(validElement);
+      return link;
+    } 
   }
 });
