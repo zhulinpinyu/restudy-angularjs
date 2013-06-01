cbedeb940659fec7bf4d087bd31bc10f06249503
zhulinpinyu <mulixiang199016@gmail.com>
2013-06-02 00:08:52 +0800

    27 templateUrl (code注意放的位置应在自定义的directive之前)

--- script/main.js
+++ script/main.js
@@ -1,18 +1,19 @@
 var app = angular.module('app', []);
 
-/*
- directive 中link的function 参数顺序不能变 即 scope element attrs
-*/
 
-app.controller('mdCtrl',function($scope){
-  console.log($scope);
-});
-
-
-app.directive('md', function(){
+app.directive('zippy', function(){
   return {
-    link: function(attrs,element, scope){
-      console.log(attrs);
-    } 
+    restrict: 'E',
+    transclude: true,
+    scope: {
+      title: '@'
+    },
+    templateUrl: 'zippy.html',
+    link: function(scope){
+      scope.isContentVisible = false;
+      scope.toggleContent = function(){
+        scope.isContentVisible = !scope.isContentVisible
+      }
+    }
   }
 });
